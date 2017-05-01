f85e277d188c1a1f24afc77d1ee6852c8791efd2
Tyler Wilding <xtvaser@gmail.com>
2017-05-01 18:10:48 -0400

    starting with sum of best component as a starting place

---
+++ TimeFormatters/RegularSumOfBestTimeFormatter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LiveSplit.TimeFormatters
+{
+    class RegularSumOfBestTimeFormatter : ITimeFormatter
+    {
+        public TimeAccuracy Accuracy { get; set; }
+        
+        public string Format(TimeSpan? time)
+        {
+            var formatter = new RegularTimeFormatter(Accuracy);
+            if (time == null)
+                return "-";
+            else
+                return formatter.Format(time);
+        }
+    }
+}
